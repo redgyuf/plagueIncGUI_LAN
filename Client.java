48bc96fdcec2964a5778829b54336fab0e7f2585
redgyuf <faramirex070@gmail.com>
2017-03-15 11:54:15 +0100

    Exit menu button implemented

--- Client.java
+++ Client.java
@@ -3,11 +3,14 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class Client {
 
 	/**
 	 * Open the window.
+	 * @wbp.parser.entryPoint
 	 */
 	public void open(String[] data) {
 		Display display = Display.getDefault();
@@ -18,14 +21,25 @@ public class Client {
 		Menu menu = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(menu);
 		
+		
+		//File menu START
 		MenuItem mntmFile = new MenuItem(menu, SWT.CASCADE);
 		mntmFile.setText("File");
 		
-		Menu menu_1 = new Menu(mntmFile);
-		mntmFile.setMenu(menu_1);
+		Menu menu_File = new Menu(mntmFile);
+		mntmFile.setMenu(menu_File);
 		
-		MenuItem mntmExit = new MenuItem(menu_1, SWT.NONE);
+		MenuItem mntmExit = new MenuItem(menu_File, SWT.NONE);
 		mntmExit.setText("Exit");
+		mntmExit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				shell.close();
+			}
+		});
+		//File menu END
+		
+		
 
 		shell.open();
 		shell.layout();
