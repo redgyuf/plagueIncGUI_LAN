88b95d4eec2f8e37eb6af28605365dce10c4a3eb
redgyuf <faramirex070@gmail.com>
2017-03-15 11:49:38 +0100

    socket receiving implemented

--- Communicator.java
+++ Communicator.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Communicator {
 	private String[] data = null;
@@ -9,4 +15,23 @@ public class Communicator {
 		this.port = port;
 	}	
 	
+	public void standByForIncData(){
+		try {
+			ServerSocket ser = new ServerSocket(port);
+			Socket soc = ser.accept();
+			
+			InputStream o = soc.getInputStream();
+	        ObjectInput s = new ObjectInputStream(o);
+	        
+	        data = (String[]) s.readObject();
+			
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public String[] getDataArray(){
+		return data;
+	}
 }
