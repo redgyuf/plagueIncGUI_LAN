d0c32a591c05543bc252cbe860596226c3904fd4
redgyuf <faramirex070@gmail.com>
2017-03-16 10:56:12 +0100

    minor update

--- Main.java
+++ Main.java
@@ -2,13 +2,16 @@ import org.eclipse.swt.SWT;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		String[] data = null;
+		Communicator cm = new Communicator("127.0.0.0", 8020);
 		
 		
 		try {
 			Client window = new Client();
-			ChooseStat popup = new ChooseStat(window);
+			ChooseStat popup = new ChooseStat();
+			window.setCm(cm);
+			popup.setMainWindow(window);
 			window.open(data, popup);
 			
 		} catch (Exception e) {
